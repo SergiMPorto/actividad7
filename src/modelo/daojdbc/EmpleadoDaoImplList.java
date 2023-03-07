451ce5eb0da4087d3bc6db2fc84f364d5e6b79b6
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2023-03-07 18:32:31 +0100

    cambios

--- src/modelo/daojdbc/EmpleadoDaoImplList.java
+++ src/modelo/daojdbc/EmpleadoDaoImplList.java
@@ -1,27 +1,101 @@
 package modelo.daojdbc;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import modelojavabeans.Empleados;
 
 public class EmpleadoDaoImplList extends AbstractDaoMy8 implements EmpleadoDao {
 
-	@Override
-	public int altaEmpleado(Empleados empleado) {
-		// TODO Auto-generated method stub
-		return 0;
+	
+		@Override
+		public int altaEmpleado(Empleados empleado) {
+		sql="insert into empleado values (?,?,?,?,?,?,?,?,?,?,?)";
+		
+		try {
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, empleado.getId_empl());
+			ps.setString(2, empleado.getNombre());
+			ps.setString(3, empleado.getApellidos());
+			ps.setCharacterStream(4, empleado.getGenero());
+			ps.setString(5, empleado.getMail());
+			ps.setString(6, empleado.getPwd());
+			ps.setDouble(7, empleado.getSalario());
+			ps.setDate(8, empleado.getFecha_ingreso());
+			ps.setDate(9, empleado.getFecha_nacimiento());
+			ps.setString(10, empleado.getId_perfil());
+			ps.setString(11, empleado.getId_depar());
+			filas = ps.executeUpdate();
+			filas=1;
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return filas;
 	}
-
-	@Override
-	public Empleados buscarUno(String id_empl) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		@Override
+		public Empleados buscarUno(String id_empl) {
+			sql="select * from empleados where id_empl = ?";
+			Empleados empleado =null;
+			try {
+				ps= conn.prepareStatement(sql);
+			    ps.setInt(1, id_empl);
+			    rs = ps.executeQuery();
+			    if(rs.next()) {
+			    	empleado = new Empleados();
+			    	empleado.setId_empl(rs.getInt("id_empl"));
+			    	empleado.setNombre(rs.getString("Nombre"));
+			    	empleado.setApellidos(rs.getString("apellidos"));
+			    	empleado.setGenero(rs.getCharacterStream("genero"));
+			    	empleado.setMail(rs.getString("email"));
+			    	empleado.setPwd(rs.getString("password"));
+			    	empleado.setSalario(rs.getDouble("salario"));
+			    	empleado.setFecha_ingreso(rs.getDate("fecha_ingreso"));
+			    	empleado.setFecha_nacimiento(rs.getDate("fecha_nacimiento"));
+			    	empleado.setId_perfil(rs.getString("id_perfil"));
+			    	empleado.setId_depar(rs.getString("id_departamento"));
+			    	
+			    }
+		} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+			return empleado;
+			}
+	
 
 	@Override
 	public List<Empleados> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		sql= "select * from empleados";
+		List<Empleados> lista = new ArrayList<> ();
+		try {
+		ps= conn.prepareStatement(sql);
+			   
+			    rs = ps.executeQuery();
+			    while(rs.next()) {
+			    	Empleados empleado = new Empleados();
+			    	empleado.setId_empl(rs.getInt("id_empl"));
+			    	empleado.setNombre(rs.getString("Nombre"));
+			    	empleado.setApellidos(rs.getString("apellidos"));
+			    	empleado.setGenero(rs.getCharacterStream("genero"));
+			    	empleado.setMail(rs.getString("email"));
+			    	empleado.setPwd(rs.getString("password"));
+			    	empleado.setSalario(rs.getDouble("salario"));
+			    	empleado.setFecha_ingreso(rs.getDate("fecha_ingreso"));
+			    	empleado.setFecha_nacimiento(rs.getDate("fecha_nacimiento"));
+			    	empleado.setId_perfil(rs.getString("id_perfil"));
+			    	empleado.setId_depar(rs.getString("id_departamento"));
+			    }
+				} catch (SQLException e) {
+						
+						e.printStackTrace();
+					}
+					return lista;
+					}
+			    	
+		
+	
 	}
 
 	@Override
@@ -52,6 +126,39 @@ public class EmpleadoDaoImplList extends AbstractDaoMy8 implements EmpleadoDao {
 	public double salarioTotalDepartamento(int id_depar) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public Empleados buscarUno(String id_empl) {
+		sql="select * from empleados where id_empl = ?";
+		Empleados empleado =null;
+		try {
+			ps= conn.prepareStatement(sql);
+		    ps.setInt(1, id_empl);
+		    rs = ps.executeQuery();
+		    if(rs.next()) {
+		    	empleado = new Empleados();
+		    	empleado.setId_empl(rs.getInt("id_empl"));
+		    	empleado.setNombre(rs.getString("Nombre"));
+		    	empleado.setApellidos(rs.getString("apellidos"));
+		    	empleado.setGenero(rs.getCharacterStream("genero"));
+		    	empleado.setMail(rs.getString("email"));
+		    	empleado.setPwd(rs.getString("password"));
+		    	empleado.setSalario(rs.getDouble("salario"));
+		    	empleado.setFecha_ingreso(rs.getDate("fecha_ingreso"));
+		    	empleado.setFecha_nacimiento(rs.getDate("fecha_nacimiento"));
+		    	empleado.setId_perfil(rs.getString("id_perfil"));
+		    	empleado.setId_depar(rs.getString("id_departamento"));
+		    	
+		    }
+	} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return empleado;
+		}
+
+	
 	}
 
 	
